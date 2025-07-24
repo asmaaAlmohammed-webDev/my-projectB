3a3a123e148240ee92ac73adb0b48a669f9661cd
asmaaAlmohammed-webDev <asmaa811almohammed@gmail.com>
2025-07-24 22:56:20 +0300

    comm

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,4 +1,5 @@
 const orderController = require('../controllers/orderController');
+const { addVarBody, addQuery } = require('../middlewares/dynamicMiddleware');
 const { protect, restrictTo } = require('./../middlewares/authMiddlewers');
 const { RoleCode } = require('./../utils/enum');
 const { USER, ADMIN } = RoleCode;
@@ -8,7 +9,14 @@ router.use(protect);
 router
   .route('/')
   .get(restrictTo(USER, ADMIN), orderController.getAllOrder)
-  .post(restrictTo(USER), orderController.createOrder);
+  .post(restrictTo(USER),addVarBody("userId","userId"), orderController.createOrder);
+router
+ .route('/mien')
+  .get(
+    restrictTo(USER),
+    addQuery('userId', 'userId'),
+    orderController.getAllOrder,
+  );
 router
   .route('/:id')
   .get(restrictTo(USER, ADMIN), orderController.getOrder)
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,13 +2,18 @@ const reviewController = require('../controllers/reviewController');
 const { protect, restrictTo } = require('./../middlewares/authMiddlewers');
 const { RoleCode } = require('./../utils/enum');
 const { USER, ADMIN } = RoleCode;
+const { addVarBody } = require('./../middlewares/dynamicMiddleware');
 const express = require('express');
 const router = express.Router();
 router.use(protect);
 router
   .route('/')
   .get(restrictTo(USER, ADMIN), reviewController.getAllReview)
-  .post(restrictTo(USER), reviewController.createReview);
+  .post(
+    restrictTo(USER),
+    addVarBody('userId', 'userId'),
+    reviewController.createReview,
+  );
 router
   .route('/:id')
   .get(restrictTo(USER, ADMIN), reviewController.getReview)
